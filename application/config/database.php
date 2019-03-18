d72e58a48912b86fa550c641e4b4193ea7fcde30
Franko <franko@plus.hr>
2019-03-18 18:35:59 +0100

    Fixed CI for PHP7, db connection as well

--- application/config/database.php
+++ application/config/database.php
@@ -49,10 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'fivasic';
-$db['default']['password'] = 'PulA1970+';
-$db['default']['database'] = 'homebooking';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'dbPassword';
+$db['default']['database'] = 'booking';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
